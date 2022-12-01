3cf3249ed1fd059c4a6573d6b30e8488738faa47
Ahmad Romy <ahmadromyzahran@gmail.com>
2022-12-02 00:20:24 +0700

    fix: subscribe button

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,7 @@
 FROM php:8.0-apache
 
 COPY . /var/www/html
+RUN apt-get update && \
+    apt-get install -y libxml2-dev
+RUN docker-php-ext-install soap
 RUN docker-php-ext-install mysqli pdo pdo_mysql
--- public/js/premium-singer.js
+++ public/js/premium-singer.js
@@ -1,10 +1,8 @@
 window.onload = function(){
     toggleSideBar();
 
-    let xhr_premium_singer = new XMLHttpRequest();
     let xhr_subscription = new XMLHttpRequest();
-    let subscribed_singer = [];
-    
+    let subscribed_singer = new Set();
     
     xhr_subscription.onreadystatechange = function() { 
         if (xhr_subscription.readyState == 4 && xhr_subscription.status == 200) {
@@ -12,39 +10,72 @@ window.onload = function(){
             let result = JSON.parse(xhr_subscription.responseText);
             if (result[0].length !== 0) {
                 for(let i=0; i<result[0].length; ++i){
-                    subscribed_singer.push(parseInt(result[0][i]['creator_id']));
+                    subscribed_singer.add(parseInt(result[0][i]['creator_id']));
                 }
             }
             console.log(subscribed_singer);
-            url_premium_singer = "http://localhost:3100/api/v1/singer/";
-        
-            xhr_premium_singer.open("GET", url_premium_singer, true);
-            xhr_premium_singer.setRequestHeader("X-Requested-With", "XMLHttpRequest");
-            xhr_premium_singer.send();
-
-            xhr_premium_singer.onreadystatechange = function() { 
-                if (xhr_premium_singer.readyState == 4 && xhr_premium_singer.status == 200) {
-                    let contents = document.getElementsByClassName("contents")[0];
-                    console.log(xhr_premium_singer.responseText);
-                    let result = JSON.parse(xhr_premium_singer.responseText);
-                    contents.innerHTML = "<tr> Unfortunately, there is no premium singer. </tr>";
-                    if (result[0] !== "") {
-                        contents.innerHTML = "";
-                        for(let i=1; i<=result.length; ++i){
-                            contents.innerHTML = generateSingers(i, result[i-1], false);
-                        }
-                        console.log(result[0]);
-                    }
-                }
-            }
+            fetchPremiumSinger1(subscribed_singer);
         }
     }
 
-    url_subscription = "/app/controllers/SubscriptionController.php?id=1";
+    url_subscription = "/app/controllers/SubscriptionController.php?status=ACCEPTED";
 
     xhr_subscription.open("GET", url_subscription, true);
     xhr_subscription.setRequestHeader("X-Requested-With", "XMLHttpRequest");
     xhr_subscription.send();
+}
+
+function fetchPremiumSinger1(subscribedSinger) {
+    let xhr_pending = new XMLHttpRequest();
+    let url = "/app/controllers/SubscriptionController.php?status=PENDING";
+    let pendingSinger = new Set();
+    xhr_pending.onreadystatechange = function() {
+        if (xhr_pending.readyState == 4 && xhr_pending.status == 200) {
+            console.log(xhr_pending.responseText);
+            let result = JSON.parse(xhr_pending.responseText);
+            if (result[0].length !== 0) {
+                for(let i=0; i<result[0].length; ++i){
+                    pendingSinger.add(parseInt(result[0][i]['creator_id']));
+                }
+            }
+            fetchPremiumSinger2(subscribedSinger, pendingSinger);
+        }
+    }
+    xhr_pending.open("GET", url, true);
+    xhr_pending.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xhr_pending.send();
+}
+
+function fetchPremiumSinger2(subscribedSinger, pendingSinger) {
+    let xhr_premium_singer = new XMLHttpRequest();
+    let url_premium_singer = "http://localhost:3100/api/v1/singer/";
+
+    xhr_premium_singer.open("GET", url_premium_singer, true);
+    xhr_premium_singer.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xhr_premium_singer.send();
+
+    xhr_premium_singer.onreadystatechange = function() { 
+        if (xhr_premium_singer.readyState == 4 && xhr_premium_singer.status == 200) {
+            let contents = document.getElementsByClassName("contents")[0];
+            console.log(xhr_premium_singer.responseText);
+            let result = JSON.parse(xhr_premium_singer.responseText);
+            contents.innerHTML = "<tr> Unfortunately, there is no premium singer. </tr>";
+            if (result[0] !== "") {
+                contents.innerHTML = "";
+                for(let i=1; i<=result.length; ++i){
+                    if(subscribedSinger.has(parseInt(result[i-1]['id']))){
+                        contents.innerHTML += generateSingers(i, result[i-1], "ACCEPTED");
+                    } else if(pendingSinger.has(parseInt(result[i-1]['id']))){
+                        contents.innerHTML += generateSingers(i, result[i-1], "PENDING");
+                    } else {
+                        contents.innerHTML += generateSingers(i, result[i-1], "canSubscribe"); //rejected or nothing
+                    }
+                }
+                console.log(result[0]);
+            }
+        }
+    }
+    
 }
 
 function toggleSideBar(){
@@ -61,9 +92,29 @@ function getDetailedSong($id){
     window.location.href = "/public/detail-song.php?id="+$id;
 }
 
-function generateSingers(i, res, isSubscribed){
-    let subscribeText = (isSubscribed) ? 'List of Songs' : 'Subscribe';
-    let subscribeClass = (isSubscribed) ? 'song' : 'sub';
+function generateSingers(i, res, status){
+    let subscribeText;
+    if(status==="ACCEPTED"){
+        subscribeText = 'List of Songs';
+    } else if(status==="PENDING"){
+        subscribeText = 'Pending';
+    } else {
+        subscribeText = 'Subscribe';
+    }
+    let subscribeClass;
+    if(status==="ACCEPTED"){
+        subscribeClass = 'song';
+    } else if(status==="PENDING"){
+        subscribeClass = 'sub disabled';
+    } else {
+        subscribeClass = 'sub';
+    }
+    let subscribeMethod;
+    if(status==="ACCEPTED"){
+        subscribeMethod = 'getPremiumSong('+res['user_id']+')'; //TODO
+    } else {
+        subscribeMethod = 'subscribe(this,'+res['user_id']+')';
+    }
     let s = `<tbody class="content-entry">
     <tr>
         <td class = 'content-id'> 
@@ -73,11 +124,34 @@ function generateSingers(i, res, isSubscribed){
             ${res['name']}
         </td>
         <td class = 'content-subs'>
-            <div class = 'button ${subscribeClass}'>
+            <div class = 'button ${subscribeClass}' onclick='${subscribeMethod}'>
                 ${subscribeText}
             </div>
         </td>
     </tr>
 </tbody>`
 return s;
+}
+
+function subscribe(elem, creator_id) {
+    let xhr = new XMLHttpRequest();
+    let url = "/app/controllers/SubscriptionController.php";
+    formData = new FormData();
+    formData.append("subscribe", true);
+    formData.append("creator_id", creator_id);
+    xhr.onreadystatechange = function() { 
+        if (xhr.readyState == 4 && xhr.status == 200) {
+            console.log(xhr.responseText);
+            let result = JSON.parse(xhr.responseText);
+            if (result[0]["SubscribeResponse"]["data"] == 1) {
+                // perlu ganti button langsung ga
+                console.log(elem);
+                elem.classList.add("disabled");
+                elem.innerHTML = "Pending";
+            }
+        }
+    }
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
+    xhr.send(formData);
 }
